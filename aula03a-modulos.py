3b3b55296f8395e8cf32fdf3275d8b6ac4109c50
davisimoncel0 <davisimoncelo@hotmail.com>
2026-03-30 08:54:25 -0300

    new feature: update

--- aula03a-modulos.py
+++ aula03a-modulos.py
@@ -1,5 +1,14 @@
 import math
+import random
 
 num = int(input("Digite um número: "))
 raiz = math.sqrt(num)
 print(f"A raiz de {num} é {raiz:.2f}")
+
+graus = 45
+radiano = graus / 180 * math.pi
+seno = math.sin(radiano)
+print(f"{seno:.2f}")
+
+num_random_int = random.randint(a=1, b=10)
+print(num_random_int)
